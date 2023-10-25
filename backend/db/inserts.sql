4e6654b3b8ffcee64c4fbc3ec1b820443768ddda
netoba1 <ernestonetoba@gmail.com>
2023-10-25 17:22:27 -0600

    proyecto

---
+++ backend/db/inserts.sql
@@ -0,0 +1,64 @@
+INSERT INTO admin (username, `password`) VALUES
+('admin1', 'password1'),
+('admin2', 'password2');
+
+INSERT INTO representante (nombre, celular, telefono, correo) VALUES
+('Representante 1', '1234567890', '9876543210', 'representante1@example.com'),
+('Representante 2', '9876543210', '1234567890', 'representante2@example.com');
+
+INSERT INTO alumno (curp, nombre, materno, paterno) VALUES
+('CURP12345678901234', 'Alumno 1', 'Materno1', 'Paterno1'),
+('CURP56789012345678', 'Alumno 2', 'Materno2', 'Paterno2');
+
+INSERT INTO `status` (descripcion) VALUES
+('Pendiente'),
+('Realizado');
+
+INSERT INTO asunto (descripcion) VALUES
+('Documentos'),
+('Informacion');
+
+INSERT INTO estado (nombre) VALUES
+('Aguascalientes'),
+('Baja California'),
+('Baja California Sur'),
+('Campeche'),
+('Chiapas'),
+('Chihuahua'),
+('Coahuila'),
+('Colima'),
+('Durango'),
+('Guanajuato'),
+('Guerrero'),
+('Hidalgo'),
+('Jalisco'),
+('México'),
+('Michoacán'),
+('Morelos'),
+('Nayarit'),
+('Nuevo León'),
+('Oaxaca'),
+('Puebla'),
+('Querétaro'),
+('Quintana Roo'),
+('San Luis Potosí'),
+('Sinaloa'),
+('Sonora'),
+('Tabasco'),
+('Tamaulipas'),
+('Tlaxcala'),
+('Veracruz'),
+('Yucatán'),
+('Zacatecas');
+
+INSERT INTO municipio (nombre, estado_id) VALUES
+('Saltillo', 7),
+('Monterrey', 18);
+
+INSERT INTO nivel (descripcion) VALUES
+('Primaria'),
+('Secundaria');
+
+INSERT INTO turno (id_representante, id_municipio, id_status, id_asunto, id_nivel, curp_alumno) VALUES
+(1, 1, 1, 1, 1, 'CURP12345678901234'),
+(2, 2, 2, 2, 2, 'CURP56789012345678');
---
+++ backend/db/script.sql
@@ -0,0 +1,87 @@
+
+CREATE DATABASE tickets_turno;
+
+USE tickets_turno;
+
+CREATE TABLE `admin` (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE representante (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL,
+  celular VARCHAR(20) NOT NULL,
+  telefono VARCHAR(20) NOT NULL,
+  correo VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE alumno (
+  curp VARCHAR(18) PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL,
+  materno VARCHAR(255) NOT NULL,
+  paterno VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `status` (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  descripcion VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE asunto (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  descripcion VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE estado (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE municipio (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(255) NOT NULL,
+  estado_id INT NOT NULL,
+  FOREIGN KEY (estado_id) REFERENCES estado(id)
+);
+
+CREATE TABLE nivel (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  descripcion VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE turno (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  id_representante INT NOT NULL,
+  id_municipio INT NOT NULL,
+  id_status INT NOT NULL,
+  id_asunto INT NOT NULL,
+  id_nivel INT NOT NULL,
+  curp_alumno VARCHAR(18) NOT NULL,
+  FOREIGN KEY (id_representante) REFERENCES representante(id),
+  FOREIGN KEY (id_municipio) REFERENCES municipio(id),
+  FOREIGN KEY (id_status) REFERENCES `status`(id),
+  FOREIGN KEY (id_asunto) REFERENCES asunto(id),
+  FOREIGN KEY (id_nivel) REFERENCES nivel(id),
+  FOREIGN KEY (curp_alumno) REFERENCES alumno(curp),
+  INDEX (curp_alumno(18))  -- Especifica la longitud máxima para la columna curp_alumno
+);
+
+CREATE VIEW VistaTurno AS
+SELECT
+    t.id,
+    r.nombre AS representante,
+    t.curp_alumno,
+    n.descripcion AS nivel,
+    m.nombre AS municipio,
+    a.descripcion AS asunto,
+    s.descripcion AS `status`
+FROM turno t
+JOIN representante r ON t.id_representante = r.id
+JOIN municipio m ON t.id_municipio = m.id
+JOIN `status` s ON t.id_status = s.id
+JOIN asunto a ON t.id_asunto = a.id
+JOIN nivel n ON t.id_nivel = n.id
+JOIN alumno al ON t.curp_alumno = al.curp;
+
